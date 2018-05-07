255d3eca42a7c6817d590b8be46e645c7b06c187
Dirk Avery <dirk.avery@gmail.com>
2018-05-07 17:09:03 -0400

    New commitish

--- tests/test_satsuki.py
+++ tests/test_satsuki.py
@@ -11,7 +11,7 @@ TEST_BODY = str(uuid.uuid1())
 TEST_SLUG = "YakDriver/satsuki"
 TEST_TAG = "Test-v" + TEST_BODY[:6]
 TEST_REL_NAME = "Test Release v" + TEST_BODY[:6]
-TEST_COMMITISH = "dd3f3843d9c691b22bd2961d5958233f3b45552f"
+TEST_COMMITISH = "c0c924bd71fe8c9f1062865f804a83be5c0cf057"
 TEST_FILENAME = 'tests/release-asset.exe'
 
 def test_blank_arguments():
